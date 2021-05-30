33743ed32b261bdb81dd24f6416d3aaf95755740
unknown <hodaia0524289665@gmail.com>
2021-05-30 20:37:00 +0300

    h

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const io=require("socket.io-client");
-var BASE_URL="https://my-socket.herokuapp.com/"
-let socket=io.connect(BASE_URL)
+var BASE_URL="https://my-socket.herokuapp.com/";
+let socket=io.connect(BASE_URL);
 
 socket.on("welcome",data=>{
 
