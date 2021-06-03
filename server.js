95afce3ec960b082c5da33119ee9ff8df9ee10c1
unknown <hodaia0524289665@gmail.com>
2021-06-03 22:42:25 +0300

    l

--- server.js
+++ server.js
@@ -5,17 +5,22 @@ const port=process.env.PORT || 8080;
 const http=require("http").createServer();
 
 const io=require("socket.io")(http);
+console.log(io);
 
 io.on("connection",(socket)=>{
 
     socket.emit("welcome","welcome to the socket")
     console.log("new client is connected!")
+    socket.on("message",(message)=>{
+
+        console.log("on message >>",message);
+    });
 });
 io.on("message", (data) => {
     console.log(data);
   });
 
-  // handle the event sent with socket.emit()
+ 
   io.on("salutations", (elem1, elem2, elem3) => {
     console.log(elem1, elem2, elem3);
   });
