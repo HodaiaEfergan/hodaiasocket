1fc76c1eaa5a40f137194512ead3d381bdf9a1ee
unknown <hodaia0524289665@gmail.com>
2021-06-03 22:03:52 +0300

    finished

--- app.js
+++ app.js
@@ -1,7 +1,42 @@
-const io=require("socket.io-client");
 var BASE_URL="https://my-socket.herokuapp.com/";
-let socket=io.connect(BASE_URL);
 
+const io = require("socket.io-client");
+
+// let socket = io.connect(BASE_URL);
+
+
+const socket = io("http://localhost:8080", {
+    reconnectionDelayMax: 10000,
+    // auth: {
+    //     token: "123"
+    // },
+    // query: {
+    //     "my-key": "my-value"
+    // }
+});
+
+
+socket.on("connect", rr => {
+    console.log("on connect", rr);
+    socket.emit("message", "This is a message from the client")
+    // socket.emit("salutations", "This is a message from the client")
+})
+
+
+// console.log(socket);
 socket.on("welcome",data=>{
     console.log("received:",data)
-});
+})
+
+socket.on("error", err => {
+     console.log("error", err);
+})
+
+socket.on("open", rr => {
+    console.log("open", rr);
+})
+
+socket.connect(res => {
+    console.log("connect", res);
+    
+})
