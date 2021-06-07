ff6db354e2f64d5b6592c715c0fabe8fb697f799
unknown <hodaia0524289665@gmail.com>
2021-06-07 09:52:20 +0300

    finish

--- server.js
+++ server.js
@@ -1,54 +1,40 @@
-// const express = require("express");
-// const app = express();
-
 const { Socket } = require("socket.io");
-
 const port = process.env.PORT || 8080;
-
-
 const fs = require('fs');
 
 const http = require("http").createServer((req, response) => {
-    response.writeHeader(200, {"Content-Type": "text/html"});  
-    const html = fs.readFileSync('./index.html');
-    response.write(html)
-    response.end();  
+  response.writeHeader(200, {"Content-Type": "text/html"});  
+  const html = fs.readFileSync('./index.html');
+  response.write(html)
+  response.end();  
 });
-
 const io = require("socket.io")(http);
-// console.log(io);
 
 io.on("connection", (socket) => {
 
-  console.info("new client is connected!")
-  socket.emit("welcome", "welcome to the socket")
+console.info("new client is connected!")
+socket.emit("welcome", "Welcome to the socket")
 
-  setInterval(() => {
-    socket.emit("time", new Date().toTimeString())
-  }, 10000000)
-
-  socket.on("message", (message) => {
-    console.log("message from cleint >>");
-    console.info(message)
-  });
-
-  socket.on("disconnect", (reason) => {
-
-    console.log("client disconnected with reason >>");
-    console.info(reason)
-  });
+socket.on("join-chat-request", (message) => {
+  console.log("message from cleint >>");
+  console.info(message)
+  handleJoinChatRequest(socket)
 
 });
 
-io.on("message", (data) => {
-  console.log(data);
+socket.on("disconnect", (reason) => {
+  console.log("client disconnected with reason >>");
+  console.info(reason)
 });
 
-// handle the event sent with socket.emit()
-io.on("salutations", (elem1, elem2, elem3) => {
-  console.log(elem1, elem2, elem3);
 });
+
+const handleJoinChatRequest = (socket) => {
+setTimeout(() => {
+  socket.emit("joined-chat", "Welcome to the Chat")
+}, 2000);
+}
 
 http.listen(port, () => {
-  console.log("server is listening on " + port)
+console.log("server is listening on " + port)
 });
